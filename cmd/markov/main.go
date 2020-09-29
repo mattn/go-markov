92103a8f19e7bb18100162dee278daf00253c674
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-09-30 00:31:19 +0900

    Add -n

--- cmd/markov/main.go
+++ cmd/markov/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -13,6 +14,10 @@ import (
 )
 
 func main() {
+	var length int
+	flag.IntVar(&length, "n", -1, "letters")
+	flag.Parse()
+
 	rand.Seed(time.Now().UnixNano())
 
 	m := markov.New()
@@ -23,5 +28,13 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(m.Chain(m.First()))
+
+	var result string
+	for {
+		result = strings.TrimSpace(m.Chain(m.First()))
+		if result != "" && (length == -1 || len([]rune(result)) <= length) {
+			break
+		}
+	}
+	fmt.Println(result)
 }
