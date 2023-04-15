452119d6204bb273f758f0d06b219452a5d346be
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-15 22:09:15 +0900

    fix

--- markov.go
+++ markov.go
@@ -3,6 +3,9 @@ package markov
 import (
 	"math/rand"
 	"regexp"
+
+	"github.com/ikawaha/kagome-dict/uni"
+	"github.com/ikawaha/kagome/v2/tokenizer"
 )
 
 var (
@@ -24,6 +27,43 @@ func (m *Markov) UpdateLine(text string) error {
 	if !ok {
 		second = make(map[string][]string)
 		m.tbl[text] = second
+	}
+	return nil
+}
+
+func (m *Markov) Update(text string) error {
+	t, err := tokenizer.New(uni.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		return err
+	}
+	text = reIgnoreText.ReplaceAllString(text, "")
+	tokens := t.Tokenize(text)
+
+	words := []string{}
+	for _, token := range tokens {
+		if token.Surface == "BOS" || token.Surface == "EOS" {
+			continue
+		}
+		words = append(words, token.Surface)
+	}
+
+	size := len(words)
+
+	if size == 1 {
+		second, ok := m.tbl[words[0]]
+		if !ok {
+			second = make(map[string][]string)
+			m.tbl[words[0]] = second
+		}
+		return nil
+	}
+	for i := 0; i < size-2; i++ {
+		second, ok := m.tbl[words[i]]
+		if !ok {
+			second = make(map[string][]string)
+			m.tbl[words[i]] = second
+		}
+		second[words[i+1]] = append(second[words[i+1]], words[i+2])
 	}
 	return nil
 }
